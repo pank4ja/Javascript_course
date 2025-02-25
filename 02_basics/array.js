f37949da55ed99cb4ac17bb856da1dc1c6ac38d8
pank4ja <drawishu@gmail.com>
2025-02-25 19:06:26 +0000

    arrays

---
+++ 02_basics/array.js
@@ -0,0 +1,45 @@
+const arr= [0,2,3,5,6,"pankaj",true];
+const arr2= ["spiderman","doremon", "shinchan"];
+const myarr =new Array(1,2,3,4,5);
+/* console.log(arr2[2]);
+
+
+console.log(arr[2]);
+
+
+// array method
+
+myarr.push(3);
+myarr.push(22);
+myarr.pop();
+myarr.unshift(100);
+myarr.shift();
+
+console.log(myarr.includes(9));
+console.log(myarr.indexOf(100));
+console.log(myarr);
+
+const newarr =myarr.join();
+console.log(newarr);
+console.log(typeof newarr);
+
+
+
+console.log(myarr); */ 
+
+
+// slice, splice
+
+console.log("A ", myarr);
+
+const myn1 = myarr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myarr);
+
+
+const myn2 = myarr.splice(1, 3)
+console.log("C ", myarr);
+console.log(myn2);
+
+
